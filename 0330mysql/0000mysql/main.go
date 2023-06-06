4e6926ab657e58fe62ad80028046eb7fe475d16d
zhanfeng.zhang <zhang.zzf@alibaba-inc.com>
2023-06-06 19:00:19 +0800

    2023-06-06 19:00

--- 0330mysql/0000mysql/main.go
+++ 0330mysql/0000mysql/main.go
@@ -9,6 +9,13 @@ import (
 
 var db *sql.DB
 
+type Album struct {
+	ID     int64
+	Title  string
+	Artist string
+	Price  float32
+}
+
 func main() {
 	cfg := mysql.Config{
 		Net:                  "tcp",
@@ -28,5 +35,32 @@ func main() {
 		log.Fatal(pingErr)
 	}
 	fmt.Println("Connected.")
+	albums, err := albumsByArtist("John Coltrane")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Albums found: %v", albums)
+}
 
+func albumsByArtist(name string) ([]Album, error) {
+	var albums []Album
+	rows, err := db.Query("select * from album where artist = ?", name)
+	if err != nil {
+		return nil, fmt.Errorf("albumsByArtist: %q: %v", name, err)
+	}
+	defer func(rows *sql.Rows) {
+		_ = rows.Close()
+	}(rows)
+	for rows.Next() {
+		var r Album
+		// 必须和列名保持一致
+		if err = rows.Scan(&r.ID, &r.Title, &r.Artist, &r.Price); err != nil {
+			return nil, fmt.Errorf("albumsByArtist: %q: %v", name, err)
+		}
+		albums = append(albums, r)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("albumsByArtist: %q: %v", name, err)
+	}
+	return albums, nil
 }
